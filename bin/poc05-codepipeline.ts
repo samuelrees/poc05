b2f76c3a66b801cf47998b327838abc1746494d3
samuelreesd <samuelreesd@gmail.com>
2021-10-11 22:25:08 -0400

    update-03

--- bin/poc05-codepipeline.ts
+++ bin/poc05-codepipeline.ts
@@ -4,7 +4,7 @@ import * as cdk from '@aws-cdk/core';
 import { Poc05CodepipelineStack } from '../lib/poc05-codepipeline-stack';
 
 const app = new cdk.App();
-new Poc05CodepipelineStack(app, 'Pocc05CodepipelineStack', {
+new Poc05CodepipelineStack(app, 'Poc05CodepipelineStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
